64b0ead865bcdef56899ca49ebbf9a80d72df982
Elton <eltonrazzini@hotmail.com>
2020-11-15 22:55:04 -0300

    classes

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php 
 
 require './class/PlacarFutebol.php';
+require './class/Liga.php';
 
 $resultados = new PlacarFutebol();
+$liga = new Liga();
 
 $paragrafoResultados = $resultados->resultadoPlacar();
+$resultadoLiga = $liga->resultadoLiga();
 
 print_r($paragrafoResultados);
+print_r($resultadoLiga);
 
